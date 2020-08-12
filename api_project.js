62b97432b20f0721dde1cb73eb1c236561e86109
sabareeshk <sabareeshk1991@gmail.com>
2020-08-12 14:25:24 +0530

    edit basic details info added

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-08-07T09:31:49.975Z",
+    "time": "2020-08-12T08:52:25.174Z",
     "url": "http://apidocjs.com",
     "version": "0.23.0"
   }
